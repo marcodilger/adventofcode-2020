eb09deff744c6be26fca6b5fe427990d95665ff0
Marco Dilger <marcodilger@gmx.de>
2020-12-09 21:01:08 +0100

    day2part2

--- src/day2.java
+++ src/day2.java
@@ -14,32 +14,31 @@ public class day2 {
         String[] passwd = line.split(" ");
         // parse min, max
         String range = passwd[0];
-        int min = Integer.parseInt(range.split("-")[0]);
-        int max = Integer.parseInt(range.split("-")[1]);
+        int pos1 = Integer.parseInt(range.split("-")[0]);
+        int pos2 = Integer.parseInt(range.split("-")[1]);
         
         // parse character of interest
         char coi = passwd[1].replace(":","").charAt(0);
         
         // count occurrences of coi
-        int count = countOccurences(passwd[2], coi, 0);
-        
-        if (count >= min & count <= max) {
+        if (passwd[2].charAt(pos1-1) == coi ^ passwd[2].charAt(pos2-1) == coi) {
             validpw++;
         }
+       
         }
         System.out.println("valid pws:" + validpw);
 
     }
-    
-    private static int countOccurences( //https://www.baeldung.com/java-count-chars
-            String someString, char searchedChar, int index) {
-              if (index >= someString.length()) {
-                  return 0;
-              }
-              
-              int count = someString.charAt(index) == searchedChar ? 1 : 0;
-              return count + countOccurences(
-                someString, searchedChar, index + 1);
-          }
+// not needed for part2   
+//    private static int countOccurences( //https://www.baeldung.com/java-count-chars
+//            String someString, char searchedChar, int index) {
+//              if (index >= someString.length()) {
+//                  return 0;
+//              }
+//              
+//              int count = someString.charAt(index) == searchedChar ? 1 : 0;
+//              return count + countOccurences(
+//                someString, searchedChar, index + 1);
+//          }
 
 }
