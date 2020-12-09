a818d5f2da11ab53bf118403eaa1c04facd2f8c4
Marco Dilger <marcodilger@gmx.de>
2020-12-09 22:12:32 +0100

    day3part2

--- src/day3.java
+++ src/day3.java
@@ -3,20 +3,37 @@ import java.util.List;
 public class day3 {
     public static void main(String[] args) {
         List<String> input = readByLine.readAsString("day3.txt");
-        int counts = 0;
         
         //get width of a line
         int width = input.get(0).length();
         
         // traverse right 3 down 1
-        for (int i = 0; i < input.size(); i++) {
-            int charPos = (i*3) % width;
-            if (input.get(i).charAt(charPos) == '#') {
-                counts++;
+        // part2:
+
+//        Right 1, down 1.
+//        Right 3, down 1. (This is the slope you already checked.)
+//        Right 5, down 1.
+//        Right 7, down 1.
+//        Right 1, down 2.
+        int right[] = {1, 3, 5, 7, 1};
+        int down[] = {1, 1, 1, 1, 2};
+        int count[] = {0, 0, 0, 0, 0};
+        for (int j = 0; j < 5;  j++) {
+            for (int i = 0; i < input.size(); i = i+down[j]) {
+                int charPos = ((i/down[j])*right[j]) % width;
+                if (input.get(i).charAt(charPos) == '#') {
+                    count[j]++;
+                }
             }
+            System.out.println(count[j]);
         }
-        System.out.println(counts);
-        
+ 
+        System.out.println(
+                count[0]*
+                count[1]*
+                count[2]*
+                count[3]*
+                count[4]);
     }
 
 }
