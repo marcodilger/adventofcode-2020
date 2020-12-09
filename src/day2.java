a702c3d123b8186fb4ed97f243f9a8f4bf23e169
Marco Dilger <marcodilger@gmx.de>
2020-12-09 18:54:54 +0100

    day2part1

---
+++ src/day2.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+
+public class day2 {
+    public static void main(String[] args) {
+        List<String> input = readByLine.readAsString("day2.txt");
+        int validpw = 0;
+        
+        //exemplary with first line, then put this in loop
+        for (String line : input) {
+            
+        
+        
+        String[] passwd = line.split(" ");
+        // parse min, max
+        String range = passwd[0];
+        int min = Integer.parseInt(range.split("-")[0]);
+        int max = Integer.parseInt(range.split("-")[1]);
+        
+        // parse character of interest
+        char coi = passwd[1].replace(":","").charAt(0);
+        
+        // count occurrences of coi
+        int count = countOccurences(passwd[2], coi, 0);
+        
+        if (count >= min & count <= max) {
+            validpw++;
+        }
+        }
+        System.out.println("valid pws:" + validpw);
+
+    }
+    
+    private static int countOccurences( //https://www.baeldung.com/java-count-chars
+            String someString, char searchedChar, int index) {
+              if (index >= someString.length()) {
+                  return 0;
+              }
+              
+              int count = someString.charAt(index) == searchedChar ? 1 : 0;
+              return count + countOccurences(
+                someString, searchedChar, index + 1);
+          }
+
+}
