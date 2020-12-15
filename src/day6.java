56f77ef5d1713824f711590a9f9500d45ff9e534
Marco Dilger <marcodilger@gmx.de>
2020-12-15 22:57:29 +0100

    day6part2

--- src/day6.java
+++ src/day6.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
 
 public class day6 {
@@ -6,28 +6,48 @@ public class day6 {
        List<String> input = readByLine.readAsString("day6.txt");
        int result = 0;
        
+       //part2: works only if nobody answeres the same question multiple times:
+       // use hashmap to count each character,
+       //keep track of no of respondees
+       // count amount of values that == no of respondees
+       
        String questions = "";
+       int respondees = 0;
        for (String line : input) {
            if (line.length() > 0) { // not an empty line
                questions = questions + line;
+               respondees++;
            } else { // empty line, sum up
-               result = result + getQuestions(questions);
+               result = result + getQuestions(questions, respondees);
                questions = "";
+               respondees = 0;
            }
        }
        //additional final sum up:
-       result = result + getQuestions(questions);
+       result = result + getQuestions(questions, respondees);
 
         System.out.println(result);
     }
     
-    public static int getQuestions(String questions) {
+    public static int getQuestions(String questions, int respondees) {
 
-        HashSet<Character> set = new HashSet<Character>();
+        HashMap<Character, Integer> hashmap = new HashMap<Character, Integer>();
         for (Character c : questions.toCharArray()) {
-                    set.add(c);
-         }
-        return(set.size());
+            if (hashmap.containsKey(c)) { // increase by 1 if present
+                hashmap.put(c, hashmap.get(c) + 1); 
+            } 
+            else {
+                hashmap.put(c, 1);  // place with 1 if not present
+            } 
+        }
+        // count characters which have a value of number of respondees
+        int result = 0;
+        for (Integer counts : hashmap.values()) {
+            if (counts == respondees) {
+                result++;
+            }
+        }
+        return result;
     }
 
 }
